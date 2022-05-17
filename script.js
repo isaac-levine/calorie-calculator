979aedc47827cece25a4b46594f1cf8fdb856ad6
isaac-levine <80065258+isaac-levine@users.noreply.github.com>
2022-05-16 20:34:01 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -47,23 +47,23 @@ form.addEventListener("submit", (e) => {
 
 
 // Calculates BMR based on Mifflin formula - assumes height in cm, weight in kg, age in years
-function mifflin(gender, age, height, weight) {
+const mifflin = (gender, age, height, weight) => {
     let base = (10 * weight) + (6.25 * height) - (5 * age);
     return gender === "female" ? base -= 161 : base += 5;
 }
 
 // converts in to cm
-function inchesToCM(inches) {
+const inchesToCM = (inches) => {
     return inches * 2.54;
 }
 
 // converts lb to kg
-function poundsToKG(pounds) {
+const poundsToKG = (pounds) => {
     return pounds * 0.45359237;
 }
 
 // calculates mantenance by scaling BMR based on activity level
-function computeMaintenance(BMR, activityLevel) {
+const computeMaintenance = (BMR, activityLevel) => {
     if (activityLevel === 'sedentary') {
         return BMR*=1.2;
     } 
@@ -83,6 +83,3 @@ function computeMaintenance(BMR, activityLevel) {
         return BMR*=1.95;
     }
 }
-
-
-
